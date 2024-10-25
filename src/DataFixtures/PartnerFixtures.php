771aae831f5ede14ff6bc0ffa8097e414d4c4f1e
Nicolas vauché <hello@nicolasvauche.net>
2024-10-25 09:53:02 +0200

    Add recaptcha in form

--- src/DataFixtures/PartnerFixtures.php
+++ src/DataFixtures/PartnerFixtures.php
@@ -25,13 +25,13 @@ class PartnerFixtures extends Fixture implements OrderedFixtureInterface
             ->setRoles(['ROLE_PARTNER'])
             ->setName('Laeti D. Photos')
             ->setActive(true)
+            ->setStatus('Actif')
             ->setType('Photo')
             ->setSpecialties('Live')
             ->setLogo('laeti-d-photos-logo.png')
             ->setDescription("Passionnée par la musique et la photographie, Laëtitia suit les groupes partout, capturant l’énergie brute des scènes et l’intensité des moments live. Toujours présente en tournée, elle immortalise chaque instant, des coulisses aux montées d’adrénaline sur scène. Photographe amateur, elle possède une solide expérience et un œil affûté pour saisir l’essence des performances. Cool et décontractée, elle se fond dans l’ambiance des concerts pour restituer des clichés authentiques, vibrants, et profondément connectés à l’univers des artistes qu’elle accompagne.")
             ->setCity('Limoges')
-            ->setZipcode('87000')
-            ->setStatus('En cours');
+            ->setZipcode('87000');
         $manager->persist($partner);
         $this->addReference('user-laeti-d-photos', $partner);
 
@@ -41,13 +41,13 @@ class PartnerFixtures extends Fixture implements OrderedFixtureInterface
             ->setRoles(['ROLE_PARTNER'])
             ->setName('Cédric Carré')
             ->setActive(true)
+            ->setStatus('Actif')
             ->setType('Photo')
             ->setSpecialties('Live')
             ->setLogo('cedric-carre-logo.png')
             ->setDescription("Photographe, illustrateur et designer basé en Charente-Maritime, ce créatif allie sourire et perfectionnisme dans chacun de ses projets. Toujours cool et accessible, il s’attache à saisir l’instant parfait, que ce soit à travers la lentille de son appareil photo ou en travaillant des illustrations et des designs uniques. Inspiré par son environnement côtier, il crée des visuels qui marient technique impeccable et sens artistique aiguisé. Soucieux du moindre détail, il travaille chaque image et chaque projet avec passion, pour offrir des résultats à la fois percutants et raffinés.")
             ->setCity('Cozes')
-            ->setZipcode('17120')
-            ->setStatus('En cours');
+            ->setZipcode('17120');
         $manager->persist($partner);
         $this->addReference('user-cedric-carre', $partner);
 
@@ -57,13 +57,13 @@ class PartnerFixtures extends Fixture implements OrderedFixtureInterface
             ->setRoles(['ROLE_PARTNER'])
             ->setName('ShadoWorkProd')
             ->setActive(true)
+            ->setStatus('Actif')
             ->setType('Vidéo')
             ->setSpecialties('Live, Clips')
             ->setDescription("ShadoWorkProd est une association audiovisuelle spécialisée dans la réalisation de clips vidéo et la captation de concerts. Discrets mais efficaces, nous œuvrons en coulisses pour sublimer vos projets et les mettre sous les feux des projecteurs. Que ce soit pour capturer l'énergie brute d'une performance live ou pour concevoir des clips créatifs et percutants, nous mettons notre savoir-faire et notre passion au service de vos idées. Avec ShadoWorkProd, votre univers artistique trouve l’écrin visuel qu’il mérite, toujours avec une touche d'authenticité&nbsp;!")
             ->setLogo('shadoworkprod-logo.png')
             ->setCity('Cressat')
-            ->setZipcode('23140')
-            ->setStatus('En cours');
+            ->setZipcode('23140');
         $manager->persist($partner);
         $this->addReference('user-shadoworkprod', $partner);
 
@@ -73,13 +73,13 @@ class PartnerFixtures extends Fixture implements OrderedFixtureInterface
             ->setRoles(['ROLE_PARTNER'])
             ->setName('Imaginaerum Studio')
             ->setActive(true)
+            ->setStatus('Actif')
             ->setType('Vidéo')
             ->setSpecialties('Live, Clips')
             ->setLogo('imaginaerum-studio-logo.jpg')
             ->setDescription("Imaginaerum Studio est une association dédiée à la production de contenu audiovisuel. En plus de cette mission, nous avons pour projet de créer un espace collaboratif dédié à la création audiovisuelle associative. Ce projet commence par l'ouverture d'un studio photo et vidéo à Aubusson, et s'étendra à terme en un complexe regroupant studios de tournage, résidences, ateliers de fabrication et bien plus. Nous souhaitons offrir aux créateurs un lieu où partager ressources, compétences et créativité pour donner vie à leurs projets dans un cadre inspirant et collaboratif.")
             ->setCity('Aubusson')
-            ->setZipcode('23200')
-            ->setStatus('En attente');
+            ->setZipcode('23200');
         $manager->persist($partner);
         $this->addReference('user-imaginaerum-studio', $partner);
 
@@ -88,14 +88,14 @@ class PartnerFixtures extends Fixture implements OrderedFixtureInterface
             ->setPassword($this->passwordHasher->hashPassword($partner, '!bEb7RgDFJM?'))
             ->setRoles(['ROLE_PARTNER'])
             ->setName('Philae Studio')
-            ->setActive(true)
+            ->setActive(false)
+            ->setStatus('Attente de proposition')
             ->setType('Studio')
             ->setSpecialties('Enregistrement, Mixage')
             ->setLogo('philae-studio-logo.jpg')
             ->setDescription("Studio d'enregistrement | Mixage | Production musicale")
             ->setCity('Limoges')
-            ->setZipcode('87000')
-            ->setStatus('En attente');
+            ->setZipcode('87000');
         $manager->persist($partner);
         $this->addReference('user-philae-studio', $partner);
 
@@ -104,14 +104,14 @@ class PartnerFixtures extends Fixture implements OrderedFixtureInterface
             ->setPassword($this->passwordHasher->hashPassword($partner, '!bEb7RgDFJM?'))
             ->setRoles(['ROLE_PARTNER'])
             ->setName('Tweed Studio')
-            ->setActive(true)
+            ->setActive(false)
+            ->setStatus('Attente de proposition')
             ->setType('Studio')
             ->setSpecialties('Enregistrement, Mixage, Mastering')
             ->setLogo('tweed-studio-logo.webp')
             ->setDescription("Studio d'enregistrement et de mastering")
             ->setCity('Montluçon')
-            ->setZipcode('03100')
-            ->setStatus('En attente');
+            ->setZipcode('03100');
         $manager->persist($partner);
         $this->addReference('user-tweed-studio', $partner);
 
